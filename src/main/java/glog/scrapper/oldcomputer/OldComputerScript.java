b47b97b542d760a0fe71fae7c2051ede044b073b
lestivalet <luiz.estivalet@trt4.jus.br>
2019-06-24 15:08:15 -0300

    add export to json for oldcomputer

--- src/main/java/glog/scrapper/oldcomputer/OldComputerScript.java
+++ src/main/java/glog/scrapper/oldcomputer/OldComputerScript.java
@@ -1,18 +1,28 @@
 package glog.scrapper.oldcomputer;
 
 import java.io.File;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.io.FileUtils;
+import org.htmlcleaner.CleanerProperties;
+import org.htmlcleaner.DomSerializer;
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.TagNode;
+import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import glog.util.XMLUtil;
+import com.google.gson.GsonBuilder;
+
 import glog.util.XMLWrapper;
 import glog.util.XPathParser;
 
@@ -32,10 +42,14 @@ public class OldComputerScript {
 		oc.setDescription(description);
 		oc.setImage(OLD_COMPUTER_URL + xp.parse("//img[contains(@src,'photos/')]/@src"));
 
-		String[] keys = { "NAME", "MANUFACTURER", "TYPE", "ORIGIN", "YEAR", "PRODUCTION", "LANGUAGE", "KEYBOARD", "CPU", "SPEED", "CO-PROCESSOR", "RAM", "VRAM", "ROM", "TEXT", "GRAPHIC", "COLORS", "SOUND", "SIZE", "PORTS", "POWER", "PRICE",
-				"BUILT", "CONTROLLERS", "MEDIA", "NUMBER", "PERIPHERALS", "SWITCHES", "BATTERIES", "BUTTONS", "GUN" };
-		String[] attrs = { "name", "manufacturer", "type", "origin", "year", "production", "language", "keyboard", "cpu", "speed", "coprocessor", "ram", "vram", "rom", "text", "graphics", "colors", "sound", "size", "ports", "power",
-				"price", "builtInGames", "controllers", "media", "numGames", "peripherals", "switches", "batteries", "buttons", "gun" };
+		String[] keys = { "NAME", "MANUFACTURER", "TYPE", "ORIGIN", "YEAR", "PRODUCTION", "LANGUAGE", "KEYBOARD", "CPU",
+				"SPEED", "CO-PROCESSOR", "RAM", "VRAM", "ROM", "TEXT", "GRAPHIC", "COLORS", "SOUND", "SIZE", "PORTS",
+				"POWER", "PRICE", "BUILT", "CONTROLLERS", "MEDIA", "NUMBER", "PERIPHERALS", "SWITCHES", "BATTERIES",
+				"BUTTONS", "GUN" };
+		String[] attrs = { "name", "manufacturer", "type", "origin", "year", "production", "language", "keyboard",
+				"cpu", "speed", "coprocessor", "ram", "vram", "rom", "text", "graphics", "colors", "sound", "size",
+				"ports", "power", "price", "builtInGames", "controllers", "media", "numGames", "peripherals",
+				"switches", "batteries", "buttons", "gun" };
 		for (int i = 0; i < keys.length; i++) {
 			String value = xp.parse("//b[contains(text(),'" + keys[i] + "')]/following::td[1]");
 			BeanUtils.setProperty(oc, attrs[i], value);
@@ -56,7 +70,8 @@ public class OldComputerScript {
 			// String src = OLD_COMPUTER_URL +
 			// images.item(i).getAttributes().getNamedItem("src").getNodeValue().toString().replace("icones_",
 			// "");
-			String src = OLD_COMPUTER_URL + images.item(i).getParentNode().getAttributes().getNamedItem("href").getNodeValue().toString().replace("icones_", "");
+			String src = OLD_COMPUTER_URL + images.item(i).getParentNode().getAttributes().getNamedItem("href")
+					.getNodeValue().toString().replace("icones_", "");
 			String description = images.item(i).getParentNode().getAttributes().getNamedItem("title").getTextContent();
 			if (description.indexOf("/title") > 0) {
 				description = description.substring(0, description.indexOf("/title"));
@@ -77,7 +92,8 @@ public class OldComputerScript {
 		}
 		NodeList images = xp.parseList("//img[contains(@src,'icones_advert')]");
 		for (int i = 0; i < images.getLength(); i++) {
-			String src = OLD_COMPUTER_URL + images.item(i).getAttributes().getNamedItem("src").getNodeValue().toString().replace("icones_", "");
+			String src = OLD_COMPUTER_URL + images.item(i).getAttributes().getNamedItem("src").getNodeValue().toString()
+					.replace("icones_", "");
 			String description = images.item(i).getParentNode().getAttributes().getNamedItem("title").getNodeValue();
 			if (description.indexOf("/title") > 0) {
 				description = description.substring(0, description.indexOf("/title"));
@@ -130,12 +146,16 @@ public class OldComputerScript {
 
 				for (int j = 1; j <= total; j++) {
 					OldComputerHardwareItem ochi = new OldComputerHardwareItem();
-					String desc = xp.parse("html/body/table[2]/tbody/tr[1]/td[3]/p[" + i + "]/following::table[" + j + "]/descendant::td[3]/p/b");
-					desc += "|||" + xp.parse("html/body/table[2]/tbody/tr[1]/td[3]/p[" + i + "]/following::table[" + j + "]/descendant::td[3]/p/text()");
-					desc += "|||" + xp.parse("html/body/table[2]/tbody/tr[1]/td[3]/p[" + i + "]/following::table[" + j + "]/descendant::td[1]/a/@title");
+					String desc = xp.parse("html/body/table[2]/tbody/tr[1]/td[3]/p[" + i + "]/following::table[" + j
+							+ "]/descendant::td[3]/p/b");
+					desc += "|||" + xp.parse("html/body/table[2]/tbody/tr[1]/td[3]/p[" + i + "]/following::table[" + j
+							+ "]/descendant::td[3]/p/text()");
+					desc += "|||" + xp.parse("html/body/table[2]/tbody/tr[1]/td[3]/p[" + i + "]/following::table[" + j
+							+ "]/descendant::td[1]/a/@title");
 					desc = desc.substring(0, desc.indexOf("/title"));
 					ochi.setDescription(desc);
-					ochi.setPhoto(OLD_COMPUTER_URL + xp.parse("html/body/table[2]/tbody/tr[1]/td[3]/p[" + i + "]/following::table[" + j + "]/descendant::td[1]/a/img/@src").replace("icones_", ""));
+					ochi.setPhoto(OLD_COMPUTER_URL + xp.parse("html/body/table[2]/tbody/tr[1]/td[3]/p[" + i
+							+ "]/following::table[" + j + "]/descendant::td[1]/a/img/@src").replace("icones_", ""));
 					och.addItem(ochi);
 				}
 				oc.addHardware(och);
@@ -175,7 +195,7 @@ public class OldComputerScript {
 		return oc;
 	}
 
-	private String getComputerInfo(int id) throws Exception {
+	private OldComputer getComputerInfo(int id) throws Exception {
 		OldComputer oc = this.summaryPage(id);
 		oc = this.photosPage(oc, id);
 		oc = this.advertPage(oc, id);
@@ -185,52 +205,92 @@ public class OldComputerScript {
 
 		this.computers.add(oc);
 
+		return oc;
+
+	}
+
+	private String toXML(OldComputer oc) throws Exception {
 		JAXBContext jc = JAXBContext.newInstance(XMLWrapper.class, OldComputer.class);
 		Marshaller marshaller = jc.createMarshaller();
 		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		StringWriter sw = new StringWriter();
+		marshaller.marshal(oc, sw);
 
-		return XMLUtil.marshal(marshaller, computers, "computers");
+		return sw.toString();
+
+	}
+
+	private String toJSON(OldComputer oc) throws Exception {
+		return new GsonBuilder().setPrettyPrinting().create().toJson(oc);
+	}
+
+	/**
+	 * Go to oldcomputers.com advanced search and execute a search without any
+	 * criteria to retrieve all links to the systems. Do it for computers, consoles
+	 * and pongs.
+	 * 
+	 * http://www.old-computers.com/search/search2.asp?st=1
+	 * 
+	 * @throws Exception
+	 */
+	private void extractLinks() throws Exception {
+		File file = new File("C:\\Users\\lestivalet\\Desktop\\computers.html");
+		String string = FileUtils.readFileToString(file);
+		TagNode tagNode = new HtmlCleaner().clean(string);
+		Document doc = new DomSerializer(new CleanerProperties()).createDOM(tagNode);
+		XPath xp = XPathFactory.newInstance().newXPath();
+		NodeList nl = (NodeList) xp.evaluate("//a[contains(@href,'computer.asp?c=')]/@href", doc,
+				XPathConstants.NODESET);
+		for (int i = 0; i < nl.getLength(); i++) {
+			System.out.println(nl.item(i).getNodeValue());
+		}
+
+	}
+
+	private void processLinks() throws Exception {
+		new File("xml/").mkdirs();
+		new File("json/").mkdirs();
+
+		List<String> lines = FileUtils.readLines(new File(
+				"C:\\Users\\lestivalet\\Downloads\\glog-scrapper-master\\data\\oldcomputer\\oldcomputer_pong.txt"));
+		for (String line : lines) {
+			OldComputerScript ocs = new OldComputerScript();
+			line = line.substring(line.indexOf("?c=") + 3, line.indexOf("&amp"));
+			System.out.println("Processing id " + line);
+			// FileUtils.write(new File(line + ".xml"),
+			// ocs.getComputerInfo(Integer.parseInt(line)));
+
+			OldComputer oc = this.getComputerInfo(Integer.parseInt(line));
+			String xml = this.toXML(oc);
+			String json = this.toJSON(oc);
+
+			FileUtils.write(new File("xml/" + line + ".xml"), xml);
+			FileUtils.write(new File("json/" + line + ".json"), json);
+		}
+
 	}
 
 	public static void main(String[] args) throws Exception {
-		// System.setProperty("http.proxyHost", "wg-vip.trt4.gov.br");
-		// System.setProperty("http.proxyPort", "3128");
-		// System.setProperty("https.proxyHost", "wg-vip.trt4.gov.br");
-		// System.setProperty("https.proxyPort", "3129");
-		// System.setProperty("http.proxyUser", "lestivalet");
-		// System.setProperty("http.proxyPassword", "LFmcc%232209");
-		// System.setProperty("https.proxyUser", "lestivalet");
-		// System.setProperty("https.proxyPassword", "LFmcc%232209");
+		System.setProperty("http.proxyHost", "wg-vip.trt4.gov.br");
+		System.setProperty("http.proxyPort", "3128");
+		System.setProperty("https.proxyHost", "wg-vip.trt4.gov.br");
+		System.setProperty("https.proxyPort", "3129");
+		System.setProperty("http.proxyUser", "downloadupdates");
+		System.setProperty("http.proxyPassword", "D0wnl04d");
+		System.setProperty("https.proxyUser", "downloadupdates");
+		System.setProperty("https.proxyPassword", "D0wnl04d");
 
 		OldComputerScript ocs = new OldComputerScript();
-		String data = ocs.getComputerInfo(1181);
-		System.out.println(data);
-		FileUtils.write(new File("test.xml"), data);
-		System.exit(0);
+		// ocs.extractLinks();
+		ocs.processLinks();
+
+		// OldComputer oc = ocs.getComputerInfo(1181);
+		// String xml = ocs.toXML(oc);
+		// System.out.println(xml);
 		//
-		// if (1 == 1)
-		// return;
-
-		// List<String> lines = FileUtils.readLines(new File("/luisoft/development/apps/glog-service/oldcomputer_pong.txt"));
-		// for (String line : lines) {
-		// OldComputerScript ocs = new OldComputerScript();
-		// line = line.substring(line.indexOf("?c=") + 3, line.indexOf("&amp"));
-		// System.out.println("Processing id " + line);
-		// FileUtils.write(new File(line + ".xml"), ocs.getComputerInfo(Integer.parseInt(line)));
-		// }
-
-		// File file = new File("/home/luisoft/Desktop/pongs.html");
-		// String string = FileUtils.readFileToString(file);
-		// TagNode tagNode = new HtmlCleaner().clean(string);
-		// Document doc = new DomSerializer(new
-		// CleanerProperties()).createDOM(tagNode);
-		// XPath xp = XPathFactory.newInstance().newXPath();
-		// NodeList nl = (NodeList)
-		// xp.evaluate("//a[contains(@href,'computer.asp?c=')]/@href", doc,
-		// XPathConstants.NODESET);
-		// for (int i = 0; i < nl.getLength(); i++) {
-		// System.out.println(nl.item(i).getNodeValue());
-		// }
+		// String json = ocs.toJSON(oc);
+		// System.out.println(json);
+		// System.exit(0);
 
 	}
 
