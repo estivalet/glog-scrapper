9b93aceaa203a3664919b7f64be7d7e5975d60ca
lestivalet <luiz.estivalet@trt4.jus.br>
2019-10-09 16:40:28 -0300

    .

--- src/main/java/glog/scrapper/oldcomputer/ImportToSQLite.java
+++ src/main/java/glog/scrapper/oldcomputer/ImportToSQLite.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.google.gson.Gson;
 
@@ -24,6 +26,7 @@ public class ImportToSQLite {
 	}
 
 	public void insert(OldComputer oc) {
+		Long systemId = 0L;
 		String sql = "INSERT INTO system (name, manufacturer, type, country, year, description, price)"
 				+ "values (?, ?, ?, ?, ?, ?, ?)";
 
@@ -36,9 +39,51 @@ public class ImportToSQLite {
 			pstmt.setString(6, oc.getDescription());
 			pstmt.setString(7, oc.getPrice());
 			pstmt.executeUpdate();
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("select last_insert_rowid()");
+			rs.next();
+			systemId = rs.getLong(1);
+			System.out.println("--->" + systemId);
+			rs.close();
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
+
+		sql = "INSERT INTO technical_information\r\n"
+				+ "(system_id, batteries, buttons, built_in_games, colors, controllers, coprocessor, cpu, graphics, gun, keyboard, language, media, num_games, peripherals, ports, power, ram, rom, size, sound, speed, switches, text, vram)\r\n"
+				+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+
+		try (Connection conn = this.connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setLong(1, systemId);
+			pstmt.setString(2, oc.getBatteries());
+			pstmt.setString(3, oc.getButtons());
+			pstmt.setString(4, oc.getBuiltInGames());
+			pstmt.setString(5, oc.getColors());
+			pstmt.setString(6, oc.getControllers());
+			pstmt.setString(7, oc.getCoprocessor());
+			pstmt.setString(8, oc.getCpu());
+			pstmt.setString(9, oc.getGraphics());
+			pstmt.setString(10, oc.getGun());
+			pstmt.setString(11, oc.getKeyboard());
+			pstmt.setString(12, oc.getLanguage());
+			pstmt.setString(13, oc.getMedia());
+			pstmt.setString(14, oc.getNumGames());
+			pstmt.setString(15, oc.getPeripherals());
+			pstmt.setString(16, oc.getPorts());
+			pstmt.setString(17, oc.getPower());
+			pstmt.setString(18, oc.getRom());
+			pstmt.setString(19, oc.getRam());
+			pstmt.setString(20, oc.getSize());
+			pstmt.setString(21, oc.getSound());
+			pstmt.setString(22, oc.getSpeed());
+			pstmt.setString(23, oc.getSwitches());
+			pstmt.setString(24, oc.getText());
+			pstmt.setString(25, oc.getVram());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 
 	public static void main(String[] args) {
